3cd6f252f44c735f03fab4993c68272ea39b00b7
Kim Stephenson & Rusty Hutto <pair+kimstephenson.rustyhutto@devbootcamp.com>
2016-10-04 15:38:43 -0700

    Write tests and functionality for displaying songs

--- spec/features/visiting_the_homepage_feature_spec.rb
+++ spec/features/visiting_the_homepage_feature_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 feature "visiting the homepage" do
   scenario "the user can see a list of songs" do
-    visit "/"
-
     song = Song.create(title: "Song", artist: "someone")
 
-    expect(page).to have_content(song.title, song.artist)
+    visit "/"
+
+    expect(page).to have_content(song.title)
+    expect(page).to have_content(song.artist)
   end
 end
