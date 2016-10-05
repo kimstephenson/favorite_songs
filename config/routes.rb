d06747b2de0dc5df318c9ee91f12d4523065d17f
Kim Stephenson <email": null>
2016-10-04 20:20:00 -0700

    Add favorites model & migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :songs, only: [:index]
+  resources :songs, only: [:index] do
+    resources :favorites, only: [:create]
+  end
 
   resources :users, only: [:new, :create]
 
